37bae5b70fa810105e8e73f1ff5e79d74effce52
James Smith <jamesplease2@gmail.com>
2018-01-31 17:15:28 -0800

    Export Fetch component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { getResources, getStatus, resourceReducer, actionTypes } from 'redux-resource';
+import { Fetch } from 'react-request';
 import Resource from './resource';
 
 export {
   Resource,
+  Fetch,
   getResources,
   getStatus,
   resourceReducer,
