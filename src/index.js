f17d08555cf6fe9a7f7f931641fca66295c72df7
Jmeas <jellyes2@gmail.com>
2018-02-03 19:09:37 -0800

    Introduce Resources component

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import {
   actionTypes
 } from 'redux-resource';
 import { Fetch } from 'react-request';
+import Resources from './resource-request';
 import ResourceRequest from './resource-request';
 
 export {
   ResourceRequest,
+  Resources,
   Fetch,
   getResources,
   getStatus,
